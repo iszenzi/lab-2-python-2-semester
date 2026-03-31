40b9ee904e557d8923920c61aecd1e71ac274a7d
ISZenzi <zenzirovi@gmail.com>
2026-03-31 21:24:02 +0300

    add new task attributes

--- src/task.py
+++ src/task.py
@@ -1,21 +1,37 @@
-from dataclasses import dataclass
 from typing import Any
+from enum import Enum
+from datetime import datetime
 from exceptions import InvalidPriorityError
+from descriptors import PriorityDescriptor
+
+
+class TaskStatus(Enum):
+    """Статусы задачи"""
+
+    NEW = "new"
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
+    FAILED = "failed"
 
 
 class Task:
-    """
-    Модель задачи
-    :param id: Идентификатор задачи
-    :param payload: Данные задачи
-    """
 
-    def __init__(self, id: int, payload: Any, priority: int, descriptons: str = ""):
-        self.id = id
+    priority = PriorityDescriptor(min_val=1, max_val=5)
+
+    def __init__(
+        self, id: int, payload: Any, priority: int, descripton: str = ""
+    ) -> None:
+        self._id = id
         self.payload = payload
         self.priority = priority
-        self.descriptons = descriptons
+        self.description = description
+        self.status = TaskStatus.NEW
+        self.created_at = datetime.now()
 
     @property
     def id(self) -> int:
         return self._id
+
+    @property
+    def ready_for_execution(self) -> bool:
+        return self.status == TaskStatus.NEW
